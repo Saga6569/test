3203a7e1160d4a20c02c385e23e853c1d6603b3c
Saga6569 <aristov6569@gmail.com>
2023-05-07 20:09:25 +0300

    1

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,59 @@
 import * as _ from 'lodash';
 import './style.css';
-import getdata from './print'
+import getdata from './print';
 
 interface Identifier {
   [key: string]: string;
 }
 
-const state = {
-  reset: false
+interface Istate {
+  requestStatus: 'progress' | 'successfully' | 'ERROR' | '';
+  arrData: Identifier[];
+};
+
+const state: Istate = {
+  requestStatus: '',
+  arrData: [],
 }
 
-const container = document.querySelector('.container')
+const slep = async () => {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+};
 
-const app = async () => {
+const container: HTMLElement = document.querySelector('.container');
+const buttondDownload: HTMLElement = document.querySelector('.download');
+const buttondReset: HTMLElement = document.querySelector('.reset');
+const plug: HTMLElement = document.querySelector('.plug');
+const load: HTMLElement = document.querySelector('.preloader');
 
+const preloader = () => {
+
+  const newClassName = `preloader_${state.requestStatus}`
+  load.className = newClassName;
+
+  setTimeout(() => {
+    load.className = 'preloader';
+  }, 2000);
+}
+
+const dataRequest = async () => {
+  state.requestStatus = 'progress'
+  preloader();
   const arrData: Identifier[] = [];
 
   for (let i = 1; i <= 5; i++) {
-    const newUrl = `https://swapi.dev/api/people/${String(i)}/`;
+
+    const randomNumber = (min: number, max: number) => Math.floor(Math.random() * (max - min + 1)) + min;
+
+    const newUrl = `https://swapi.dev/api/people/${String(randomNumber(1, 81))}/`;
     const res: Identifier = await getdata(newUrl);
-    // console.log(res)
+
+    if (res.status === 'ERROR') {
+      state.requestStatus = 'ERROR'
+      preloader()
+      return res;
+    };
+
     const keys = Object.keys(res);
 
     const newDate: Identifier = {};
@@ -30,40 +64,131 @@ const app = async () => {
       };
     });
     arrData.push(newDate);
-  }
+  };
 
+  // await slep()
 
-  const keys = Object.keys(arrData[0])
+  state.requestStatus = 'successfully';
+  localStorage.setItem("arrData", JSON.stringify(arrData));
 
-  const buttondDownload = document.querySelector('.download')
-  buttondDownload.addEventListener('click', (e: Event, reset = false) => {
+  preloader();
+  state.arrData = arrData;
+}
 
-    reset = state.reset
+const onClick = async () => {
+  if (state.arrData.length === 0) {
+    const request = await dataRequest()
+    if (state.requestStatus === 'ERROR') {
+      return;
+    };
+  };
 
-    const heder = document.querySelector('.name') as HTMLElement
-    const childHeder = Array.from(heder.children) as HTMLElement[]
+  const arrData = state.arrData;
+  const keys = Object.keys(arrData[0]);
+  const heder = document.querySelector('.name') as HTMLElement;
+  const childHeder = Array.from(heder.children) as HTMLElement[];
 
-    keys.forEach((key: string, i: number) => {
-      if (i === 0) {
-        return
+  keys.forEach((key: string, i: number) => {
+    if (i === 0) {
+      return;
+    };
+
+    const line = document.querySelector(`.line_${i}`) as HTMLElement;
+    const childLine = Array.from(line.children);
+    childLine.forEach((line: HTMLElement, j: number) => {
+      if (j === 0) {
+        line.innerText = key;
+        const divDrag = document.createElement('div');
+        divDrag.className = 'dragSlider'
+        line.append(divDrag)
+        return;
       }
-      const line = document.querySelector(`.line_${i}`) as HTMLElement
-      const childLine = Array.from(line.children);
-      childLine.forEach((el: HTMLElement, j: number) => {
-        if (j === 0) {
-          el.innerText = reset ? '' : key
-          return
-        }
-        childHeder[j].innerText = reset ? '' : arrData[j - 1].name
-        el.innerText = reset ? '' :  arrData[j - 1][key]
-      })
-    })
+      childHeder[j].innerText = arrData[j - 1].name;
+      line.innerText = arrData[j - 1][key];
+    });
+  });
 
-    // setTimeout(() => {
-    //   state.reset = true
-    // }, 5000);
+  buttondDownload.setAttribute('disabled', 'false');
+  container.style.opacity = '1';
+  container.style.transition = '2s';
+  buttondReset.style.display = '';
+  plug.style.display = 'none';
+};
 
+const onClickRes = async (e: Event) => {
+
+  const heder = document.querySelector('.name') as HTMLElement;
+  const childHeder = Array.from(heder.children) as HTMLElement[];
+
+  childHeder.forEach((el: HTMLElement, i: number) => {
+    el.innerText = i === 0 ? el.innerText : '';
   })
 
-}
-app()
+  const arrTr = Array.from(document.querySelectorAll('tr')) as HTMLElement[];
+  arrTr.forEach((el: HTMLElement, i: number) => {
+    if (i === 0) {
+      return;
+    }
+    Array.from(el.children).forEach((element: HTMLElement, j: number) => {
+      if (j === 0) {
+        return;
+      }
+      element.innerText = '';
+    });
+  });
+  buttondDownload.removeAttribute('disabled');
+  container.style.opacity = '0'
+  buttondReset.style.display = 'none';
+  plug.style.display = '';
+  localStorage.setItem("arrData", JSON.stringify(null));
+  state.arrData = []
+};
+
+
+const dragApp = () => {
+
+  const arrTr: HTMLElement[] = Array.from(document.querySelectorAll('tr'));
+
+  arrTr.forEach((el: HTMLElement) => {
+    if (el.className === 'name') {
+      return;
+    };
+    el.addEventListener("dragstart", (event: any) => {
+    });
+    el.addEventListener("dragenter", (event: any) => {
+    });
+    el.addEventListener("dragLeave", (event: any) => {
+    });
+    el.addEventListener("drag", (event: any) => {
+    });
+
+
+
+  });
+
+};
+
+const app = async () => {
+  buttondDownload.addEventListener('click', () => onClick());
+  buttondReset.addEventListener('click', onClickRes);
+
+  const localStorageData = JSON.parse(localStorage.getItem('arrData'));
+
+  dragApp();
+
+  if (localStorageData !== null) {
+    state.arrData = localStorageData;
+    plug.style.display = 'none';
+    container.style.opacity = '1';
+    container.style.transition = '2s';
+    onClick();
+    return;
+  };
+
+  container.style.opacity = '0';
+  container.style.transition = '0s';
+  buttondReset.style.display = 'none';
+
+};
+
+app();
--- src/print.ts
+++ src/print.ts
@@ -1,21 +1,18 @@
 const getdata = async (url: string) => {
-    try {
-        const response = await fetch(url);
-        console.log(response)
-        if (response.status !== 200) {
-            return response.status
-        }
-        if (response === undefined) {
-            return 'ошибка сети'
-        }
-        let json = await response.json()
-        return json
-    } catch (er) {
-        return er
+  try {
+    const response = await fetch(url);
+    if (response.status !== 200) {
+      return { status: 'ERROR', value: response.status }
     }
-
+    if (response === undefined) {
+      return { status: 'ERROR', value: 'ошибка сети' }
+    }
+    let json = await response.json()
+    return json
+  } catch (error) {
+    console.log(error)
+    return { status: 'ERROR', value: error }
+  }
 }
-// console.log(getdata(`https://swapi.dev/api/people/1/`))
-
 
 export default getdata;
